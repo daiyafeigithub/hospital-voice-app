d7d60e6693bd37977342e80c6f4d619a85577acd
daiyafei <simon.dyf@qq.com>
2026-06-09 11:50:57 +0800

    add certs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.45', 'localhost'],
+  allowedDevOrigins: ['https://118.195.146.26', 'https://192.168.1.45', 'https://localhost', 'http://localhost'],
 };
 
 export default nextConfig;
